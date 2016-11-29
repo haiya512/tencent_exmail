872e6fea76f4f7a3f3771f1d6f700c906b4af4e6
haiya512 <haiya512@users.noreply.github.com>
2016-11-29 16:11:45 +0800

    修改了headers
    修改了headers
    对应的python版本2.7.10

--- get_tencent_exmail_contacts.py
+++ get_tencent_exmail_contacts.py
@@ -86,10 +86,9 @@ if __name__ == "__main__":
     headers = {'Connection': 'keep-alive',
                'Cache-Control': 'max-age=0',
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-               'Upgrade-Insecure-Requests': 1,
+               'Upgrade-Insecure-Requests': '1',
                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
-               'DNT': 1,
-               'Accept-Encoding': 'gzip, deflate, sdch',
+               'Accept-Encoding': 'gzip, deflate, br',
                'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4',
                }
     resp = session.get('http://exmail.qq.com/login', headers=headers)
